b3e27a9d052403a4b8dbf19973c9d6595b2efaf0
Shahked Bleicher <shahkedb4@gmail.com>
2018-04-24 23:23:43 -0400

    Testing what the input actually is

--- src/main/java/com/aws/codestar/projecttemplates/handler/MatrixHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/MatrixHandler.java
@@ -1,20 +1,51 @@
 package com.aws.codestar.projecttemplates.handler;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.json.JSONObject;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.aws.codestar.projecttemplates.GatewayResponse;
 
-public class MatrixHandler implements RequestHandler<Object, Object> {
-	public Object handleRequest(final Object input, final Context context) {
+public class MatrixHandler implements RequestHandler<Map<String, Integer>, Object> {
+	
+	public Object handleRequest(Map<String, Integer> params, final Context context) {
     	Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        JSONObject responseBody = new JSONObject().put("row1", "1|2|3|4|5");
         
-        return new GatewayResponse(responseBody.toString(), headers, 200);
+        JSONObject responseBody = new JSONObject();//.put("row1", "1|2|3|4|5");
+        File dataFile = new File("src/main/resources/data.txt");
+        
+        /*Map<String, Integer> params = Arrays.stream(input.split("&"))
+        		.map(param -> param.split("="))
+        		.collect(
+        			Collectors.toMap(param -> param[0], param -> Integer.parseInt(param[1]))
+        		);*/
+        
+        if (params.get("col1") != params.get("row2")) {
+        	responseBody.put("mess", "“incompatible	matrices");
+        	return new GatewayResponse(responseBody.toString(), headers, 200);
+        }
+        
+        if (!dataFile.isFile()) {
+        	responseBody.put("mess", "The data file does not exist");
+        	return new GatewayResponse(responseBody.toString(), headers, 200);
+        }
+        
+        return new GatewayResponse(responseBody.put("params", params.toString()).toString(), headers, 200);
+        
+        
+        
+        
     }
+	
+	public void multiply() {
+		
+	}
 }
